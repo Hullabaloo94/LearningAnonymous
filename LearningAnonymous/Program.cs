31d236e37a234cbedaab856d7ea04e8a7506ef01
Jason Hall <jasonahall1994@gmail.com>
2019-11-14 12:15:17 +0000

    initial commit

---
+++ LearningAnonymous/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace LearningAnonymous
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("downloading file");
+            Download();
+            Console.ReadLine();
+
+        }
+
+        static void Download()
+        {
+            Network.Download((message) => Console.WriteLine("File Downloaded" + message));
+        }
+    }
+
+    // Imaginary external network library
+
+    class Network
+    {
+        static public void Download(Action<String> callback)
+        {
+            Task.Run(() =>
+            {
+                Thread.Sleep(3000);
+                callback("completed");
+            });
+        }
+    }
+
+}
